8b90f5b34270104757108958202e855ed9d77cf8
ABandonLoves <a_bandon_love@outlook.com>
2026-08-06 04:40:40 +0800

    无限挑战

--- src/core/secret-formula/challenges/infinity-challenges.js
+++ src/core/secret-formula/challenges/infinity-challenges.js
@@ -1,12 +1,11 @@
 export const infinityChallenges = [
   {
     id: 1,
-    description: `all Normal Challenge restrictions are active at once, with the exception of the
-      Tickspeed (C9) and Big Crunch (C12) Challenges.`,
+    description: `同时进行所有的普通挑战 (挑战9和挑战12除外)。`,
     goal: DC.E650,
     isQuickResettable: true,
     reward: {
-      description: () => `${formatX(2.3, 1, 1)} on all Infinity Dimensions for each Infinity Challenge completed`,
+      description: () => `每个完成的无限挑战提供 ${formatX(2.3, 1, 1)} 的无限维度倍率`,
       effect: () => Math.pow(2.3, InfinityChallenges.completed.length),
       formatEffect: value => formatX(value, 1, 1)
     },
@@ -14,12 +13,11 @@ export const infinityChallenges = [
   },
   {
     id: 2,
-    description: () => `Dimensional Sacrifice happens automatically every ${formatInt(400)} milliseconds once you have
-      an 8th Antimatter Dimension.`,
+    description: () => `在你拥有第八反物质维度之后，每隔 ${formatInt(400)} 自动进行一次维度献祭。`,
     goal: DC.E10500,
     isQuickResettable: false,
     reward: {
-      description: () => `Dimensional Sacrifice autobuyer and stronger Dimensional Sacrifice
+      description: () => `解锁自动献祭，并获得更强的献祭加成
         ${Sacrifice.getSacrificeDescription({ "InfinityChallenge2isCompleted": false })} ➜
         ${Sacrifice.getSacrificeDescription({ "InfinityChallenge2isCompleted": true })}`,
     },
@@ -28,14 +26,14 @@ export const infinityChallenges = [
   {
     id: 3,
     description: () =>
-      `Tickspeed upgrades are always ${formatX(1)}. For every Tickspeed upgrade purchase, you instead get a static
-      multiplier on all Antimatter Dimensions which increases based on Antimatter Galaxies.`,
+      `计数频率的升级总是增加 ${formatX(1)} 。
+      但你每次购买计数频率升级时，你的反物质维度会获得一个基于反物质星系数量的倍数加成。`,
     goal: DC.E5000,
     isQuickResettable: false,
     effect: () => Decimal.pow(player.galaxies.times(0.005).add(1.05), player.totalTickBought),
     formatEffect: value => formatX(value, 2, 2),
     reward: {
-      description: `Antimatter Dimension multiplier based on Antimatter Galaxies and Tickspeed purchases`,
+      description: `反物质维度获得基于反物质星系和计数频率升级的购买数量的加成`,
       effect: () => (Laitela.continuumActive
         ? Decimal.pow(player.galaxies.times(0.005).add(1.05), Tickspeed.continuumValue)
         : Decimal.pow(player.galaxies.times(0.005).add(1.05), player.totalTickBought)),
@@ -46,13 +44,13 @@ export const infinityChallenges = [
   {
     id: 4,
     description: () =>
-      `only the latest bought Antimatter Dimension's production is normal. All other Antimatter Dimensions
-      produce less (${formatPow(0.25, 2, 2)}).`,
+      `只有最近一次购买的反物质维度可以正常生产。
+      所有其他的反物质维度的产量将下降 (${formatPow(0.25, 2, 2)})。`,
     goal: DC.E13000,
     isQuickResettable: true,
     effect: 0.25,
     reward: {
-      description: () => `All Antimatter Dimension multipliers become multiplier${formatPow(1.05, 2, 2)}`,
+      description: () => `所有反物质维度倍数加成变成原来的 ${formatPow(1.05, 2, 2)} `,
       effect: 1.05
     },
     unlockAM: DC.E14000,
@@ -60,14 +58,14 @@ export const infinityChallenges = [
   {
     id: 5,
     description:
-      `buying Antimatter Dimensions 1-4 causes all cheaper AD costs to increase.
-      Buying Antimatter Dimensions 5-8 causes all more expensive AD costs to increase.`,
+      `购买第 1-4 反物质维度时，会增加所有价格较小或相等的反物质维度价格。
+      购买第 5-8 反物质维度时，会增加所有价格较大或相等的反物质维度价格。`,
     goal: DC.E16500,
     isQuickResettable: true,
     reward: {
       description: () =>
-        `All Galaxies are ${formatPercents(0.1)} stronger and reduce the requirements for them
-        and Dimension Boosts by ${formatInt(1)}`,
+        `星系的加成提升 ${formatPercents(0.1)} ，
+        维度提升和反物质星系的需求减少 ${formatInt(1)} 。`,
       effect: 1.1
     },
     unlockAM: DC.E18000,
@@ -75,14 +73,13 @@ export const infinityChallenges = [
   {
     id: 6,
     description: () =>
-      `exponentially rising matter divides the multiplier on all of your Antimatter Dimensions
-      once you have at least ${formatInt(1)} 2nd Antimatter Dimension.`,
+      `一旦你拥有了 ${formatInt(1)} 个第二反物质维度，反物质维度的倍数加成就会被除以指数级增加的物质数量。`,
     goal: DC.D2E22222,
     isQuickResettable: true,
     effect: () => Currency.matter.value.clampMin(1),
     formatEffect: value => `/${format(value, 1, 2)}`,
     reward: {
-      description: "Infinity Dimension multiplier based on tickspeed",
+      description: "无限维度获得基于计数频率的加成",
       effect: () => Tickspeed.perSecond.pow(0.0005),
       formatEffect: value => formatX(value, 2, 2)
     },
@@ -100,14 +97,14 @@ export const infinityChallenges = [
         InfinityChallenge(7).reward,
         TimeStudy(81)
       );
-      return `you cannot buy Antimatter Galaxies. Base Dimension Boost multiplier is increased to a maximum
-        of ${formatX(10)}. (Current base multiplier: ${formatX(mult, 2, 1)})`;
+      return `你不能获得反物质星系，但维度提升的倍数加成增加，最高提升至 ${formatX(10)} 。
+      (当前: ${formatX(mult, 2, 1)} )`;
     },
     goal: DC.E10000,
     isQuickResettable: false,
     effect: 10,
     reward: {
-      description: () => `Dimension Boost multiplier is increased to a minimum of ${formatX(4)}`,
+      description: () => `维度提升的倍数加成提高到至少 ${formatX(4)}`,
       effect: 4
     },
     unlockAM: DC.E23000,
@@ -115,15 +112,14 @@ export const infinityChallenges = [
   {
     id: 8,
     description: () =>
-      `AD production rapidly and continually drops over time. Purchasing Antimatter Dimension or Tickspeed
-        upgrades sets production back to ${formatPercents(1)} before it starts dropping again.`,
+      `购买任何东西后，你的产量会恢复为 ${formatPercents(1)} ，并从此开始迅速下降。`,
     goal: DC.E27000,
     isQuickResettable: true,
     effect: () => DC.D0_8446303389034288.pow(
       Decimal.max(0, player.records.thisInfinity.time.sub(player.records.thisInfinity.lastBuyTime))),
     reward: {
       description:
-        "You get a multiplier to AD 2-7 based on 1st and 8th AD multipliers.",
+        "基于第一和第八维度的倍数，获得第二至第七维度额外倍数",
       effect: () => AntimatterDimension(1).multiplier.times(AntimatterDimension(8).multiplier).pow(0.02).clampMax(DC.E1E15.powEffectsOf(EndgameMastery(91), EndgameUpgrade(11))).pow(Decimal.max(Decimal.pow(5, Decimal.log10(Decimal.log10(AntimatterDimension(1).multiplier.times(AntimatterDimension(8).multiplier).pow(0.02)).div(Decimal.log10(DC.E1E15.powEffectsOf(EndgameMastery(91), EndgameUpgrade(11)))))), 1)),
       cap: () => Alpha.isDestroyed ? DC.BEMAX : DC.E1E15.powEffectsOf(EndgameMastery(91), EndgameUpgrade(11)),
       formatEffect: value => formatX(value, 2, 2)
