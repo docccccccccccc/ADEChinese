9b795d1a0eb9e3c69148e8948b0884649ba44461
ABandonLoves <a_bandon_love@outlook.com>
2026-08-10 03:23:04 +0800

    天界无限升级

--- src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-break-infinity-upgrades.js
+++ src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-break-infinity-upgrades.js
@@ -25,40 +25,40 @@ export const celestialBreakInfinityUpgrades = {
   autoCD1: {
     id: "autoCD1",
     cost: 5e4,
-    description: "Unlock Autobuyers for Celestial Dimensions 1-4"
+    description: "解锁第1-4天界维度自动购买器"
   },
   autoCD2: {
     id: "autoCD2",
     cost: 1e5,
-    description: "Unlock Autobuyers for Celestial Dimensions 5-8"
+    description: "解锁第5-8天界维度自动购买器"
   },
   autoCDPlus: {
     id: "autoCDPlus",
     cost: 1e6,
-    description: "Unlock Autobuyers for Celestial Tickspeed, Celestial Dimension Boosts, Celestial Galaxies and Celestial Crunches"
+    description: "解锁天界维度的计数频率、维度提升、星系和大坍缩的自动购买器"
   },
   betterAuto: {
     id: "betterAuto",
     cost: 1e9,
-    description: () => `All Celestial Dimension-related Automation is ${formatX(3)} faster`,
+    description: () => `天界自动购买器速度提升 ${formatX(3)} 倍`,
     effect: 3
   },
   bulkCelDimBoosts: {
     id: "bulkCelDimBoosts",
     cost: 1e15,
-    description: "Unlock the buy max Celestial Dimension Boost Autobuyer mode"
+    description: "天界维度提升自动购买器可购买最大"
   },
   celInfGen: {
     id: "celInfGen",
     cost: 1e24,
-    description: () => `Generate Celestial Infinities at ${formatPercents(0.5)} of your fastest`
+    description: () => `基于最快的天界无限速度的 ${formatPercents(0.5)} 生成天界无限次数`
   },
   celTickspeedCostMult: rebuyable({
     id: 0,
     initialCost: 1e5,
     costIncrease: 20,
     maxUpgrades: 8,
-    description: "Reduce post-infinity Celestial Tickspeed Upgrade cost multiplier scaling",
+    description: "降低天界无限后天界计数频率升级的价格增长倍率",
     noLabel: true,
     onPurchased: () => GameCache.celestialTickSpeedMultDecrease.invalidate()
   }),
@@ -67,7 +67,7 @@ export const celestialBreakInfinityUpgrades = {
     initialCost: 4e5,
     costIncrease: 100,
     maxUpgrades: 7,
-    description: "Reduce post-infinity Celestial Dimension cost multiplier scaling",
+    description: "降低天界无限后天界维度提升升级的价格增长倍率",
     noLabel: true,
     onPurchased: () => GameCache.celestialDimensionMultDecrease.invalidate()
   }),
@@ -78,11 +78,11 @@ export const celestialBreakInfinityUpgrades = {
     maxUpgrades: 10,
     effect: value => player.disablePostReality ? DC.D0 : Player.bestRunCIPPM.times(value / 20),
     description: () => {
-      let generation = `Generate ${formatInt(5 * player.endgame.celDimExpansion.celestialInfinityRebuyables[2])}%`;
+      let generation = `${formatInt(5 * player.endgame.celDimExpansion.celestialInfinityRebuyables[2])}%`;
       if (!CelestialBreakInfinityUpgrade.cipGen.isCapped) {
         generation += ` ➜ ${formatInt(5 * (1 + player.endgame.celDimExpansion.celestialInfinityRebuyables[2]))}%`;
       }
-      return `${generation} of your best CIP/min from your last 10 Celestial Infinities`;
+      return `基于前 10 次天界无限中最快天界无限点数获取速度的 ${generation} 生成天界无限点数`;
     },
     isDisabled: effect => effect.eq(0),
     formatEffect: value => `${format(value, 2, 1)} CIP/min`,
@@ -94,7 +94,7 @@ export const celestialBreakInfinityUpgrades = {
     costIncrease: 1e3,
     maxUpgrades: 10,
     effect: value => player.disablePostReality ? 1 : Math.pow(1.2, value),
-    description: () => `Increase the Celestial Dimension per-purchase multiplier by ${formatPercents(0.2)}`,
+    description: () => `将每次购买天界维度的倍率提高 ${formatPercents(0.2)}`,
     formatEffect: value => `${formatX(value, 2, 2)}`,
     noLabel: false
   }),
@@ -104,7 +104,7 @@ export const celestialBreakInfinityUpgrades = {
     costIncrease: 1e6,
     maxUpgrades: 10,
     effect: value => player.disablePostReality ? 1 : Math.pow(1.5, value),
-    description: () => `Increase the Celestial Dimension Boost multiplier by ${formatPercents(0.5)}`,
+    description: () => `将每次天界维度提升的倍率提高 ${formatPercents(0.5)}`,
     formatEffect: value => `${formatX(value, 2, 2)}`,
     noLabel: false
   }),
@@ -114,7 +114,7 @@ export const celestialBreakInfinityUpgrades = {
     costIncrease: 1e9,
     maxUpgrades: 10,
     effect: value => player.disablePostReality ? 1 : Math.pow(1.1, value),
-    description: () => `Increase the Celestial Galaxy to Celestial Tickspeed multiplier by ${formatPercents(0.1)}`,
+    description: () => `天界星系对每个天界计数频率的加成 + ${formatPercents(0.1)}`,
     formatEffect: value => `${formatX(value, 2, 2)}`,
     noLabel: false
   })
