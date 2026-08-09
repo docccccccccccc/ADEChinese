67a7fd85eedf667c8192c53c1cf9e1d40914226f
ABandonLoves <a_bandon_love@outlook.com>
2026-08-10 03:41:22 +0800

    天界永恒升级

--- src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-eternity-upgrades.js
+++ src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-eternity-upgrades.js
@@ -24,7 +24,7 @@ export const celestialEternityUpgrades = {
     costIncrease: 1e4,
     maxUpgrades: 10,
     effect: value => player.disablePostReality ? 1 : Math.pow(0.99, value),
-    description: () => `Reduce the Celestial Infinity Point conversion formula divisor by ${formatPercents(0.01)}`,
+    description: () => `将天界无限点数转换公式的除数减少 ${formatPercents(0.01)}`,
     formatEffect: value => `${formatX(value, 2, 3)}`,
     noLabel: false
   }),
@@ -34,7 +34,7 @@ export const celestialEternityUpgrades = {
     costIncrease: 10,
     maxUpgrades: 1000,
     effect: value => player.disablePostReality ? DC.D1 : Decimal.pow(1000, value),
-    description: () => `Multiply Celestial Dimensions by ${formatX(1000)} per purchase`,
+    description: () => `提升天界维度的购买倍率至 ${formatX(1000)}`,
     formatEffect: value => `${formatX(value, 2, 2)}`,
     noLabel: false
   }),
@@ -44,87 +44,86 @@ export const celestialEternityUpgrades = {
     costIncrease: 1e50,
     maxUpgrades: 25,
     effect: value => player.disablePostReality ? 1 : Math.pow(1.01, value),
-    description: () => `Multiply the Celestial Dimension Conversion Exponent by ${formatX(1.01, 2, 2)}`,
+    description: () => `将天界物质激发指数乘以 ${formatX(1.01, 2, 2)}`,
     formatEffect: value => `${formatX(value, 2, 3)}`,
     noLabel: false
   }),
   startBreak: {
     id: "startBreak",
     cost: 10,
-    description: "Start Celestial Eternities with Celestial Infinity Broken"
+    description: "开始天界永恒时打破天界无限"
   },
   bulkCelGalaxies: {
     id: "bulkCelGalaxies",
     cost: 1e3,
-    description: "Unlock the buy max Celestial Galaxy Autobuyer mode"
+    description: "天界星系自动购买器可购买最大"
   },
   instaAutos: {
     id: "instaAutos",
     cost: 1e6,
-    description: "Celestial Dimension-related Autobuyer intervals are now Instant"
+    description: "天界自动购买器间隔降低为立刻"
   },
   x2CIPAuto: {
     id: "x2CIPAuto",
     cost: 1e10,
-    description: () => `Unlock an Autobuyer for the ${formatX(2)} CIP Multiplier Upgrade`
+    description: () => `解锁 ${formatX(2)} 天界无限点数升级自动购买器`
   },
   betterCelCrunchAuto: {
     id: "betterCelCrunchAuto",
     cost: 1e15,
-    description: "Improve the Celestial Crunch Autobuyer"
+    description: "解锁多样化自动天界大坍缩"
   },
   startInf: {
     id: "startInf",
     cost: 1e20,
-    description: "Start with all Celestial Infinity Upgrades bought"
+    description: "开始时自动解锁所有天界无限升级"
   },
   startingBoosts: {
     id: "startingBoosts",
     cost: 1e30,
-    description: () => `Start Cel Crunches and Cel Eternities with ${format(5e25, 2, 2)} Cel Matter, and Cel Eternities
-      with ${format(5e25, 2, 2)} CIP`,
+    description: () => `天界无限和天界永恒后初始拥有 ${format(5e25, 2, 2)} 天界物质，天界永恒后初始拥有 ${format(5e25, 2, 2)} 天界无限点数`,
     effect: 5e25
   },
   startBreakInf: {
     id: "startBreakInf",
     cost: 1e40,
-    description: "Start with all Celestial Break Infinity Upgrades bought"
+    description: "开始时自动解锁所有打破天界无限升级"
   },
   celEternityAuto: {
     id: "celEternityAuto",
     cost: 1e50,
-    description: "Unlock an Autobuyer for Celestial Eternities"
+    description: "解锁天界永恒自动购买器"
   },
   freeDimBoost: {
     id: "freeDimBoost",
     cost: 1e65,
-    description: "Purchasing Celestial Dimension Boosts no longer resets anything"
+    description: "购买天界维度提升不再重置任何东西"
   },
   freeGalaxy: {
     id: "freeGalaxy",
     cost: 1e80,
-    description: "Purchasing Celestial Galaxies no longer resets anything"
+    description: "购买天界星系不再重置任何东西"
   },
   betterCelEternityAuto: {
     id: "betterCelEternityAuto",
     cost: 1e100,
-    description: "Improve the Celestial Eternity Autobuyer"
+    description: "解锁多样化自动天界永恒"
   },
   celTickReduction: {
     id: "celTickReduction",
     cost: 1e150,
-    description: () => `Reduce the post-Break Celestial Tickspeed cost scaling to ${formatX(1.65, 2, 2)}`,
+    description: () => `将打破天界无限后天界计数频率的价格增长降低至 ${formatX(1.65, 2, 2)}`,
     effect: 0.35
   },
   celDimReduction: {
     id: "celDimReduction",
     cost: 1e225,
-    description: () => `Reduce the post-Break Celestial Dimension cost scaling to ${formatX(2)}`,
+    description: () => `将打破天界无限后天界维度的价格增长降低至${formatX(2)}`,
     effect: 1
   },
   passiveCIP: {
     id: "passiveCIP",
     cost: 1e300,
-    description: () => `Generate ${formatPercents(0.01)} of your pending CIP every second`
+    description: () => `每秒生成天界大坍缩后所能获得的天界无限点数的 ${formatPercents(0.01)}`
   },
 };
