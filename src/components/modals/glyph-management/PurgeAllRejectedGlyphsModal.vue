74d9312ee4580197774810d80e9bf66cd8f249a6
ABandonLoves <a_bandon_love@outlook.com>
2026-08-11 22:48:01 +0800

    vue：献祭精炼符文

--- src/components/modals/glyph-management/PurgeAllRejectedGlyphsModal.vue
+++ src/components/modals/glyph-management/PurgeAllRejectedGlyphsModal.vue
@@ -13,24 +13,22 @@ export default {
   },
   computed: {
     refiningOrSacrificing() {
-      if (this.isRefining) return `Refine`;
-      return `Sacrifice`;
+      if (this.isRefining) return `精炼`;
+      return `献祭`;
     },
     topLabel() {
-      return `You are about to ${this.refiningOrSacrificing} all rejected Glyphs`;
+      return `你将要${this.refiningOrSacrificing}所有不符合筛选条件的符文`;
     },
     message() {
       const negativeWarning = AutoGlyphProcessor.hasNegativeEffectScore()
-        ? ` Note that some of your Effect Filter scores are negative, which may cause you to lose some Glyphs
-          you normally want to keep.`
+        ? `需注意：部分效果筛选得分为负值，可能导致你意外失去通常想保留的符文。`
         : "";
-      return `Are you sure you want to ${this.refiningOrSacrificing} all rejected Glyphs? This will remove
-        all Glyphs that would be rejected by your current Glyph Filter settings.${negativeWarning}`;
+      return `你确定要${this.refiningOrSacrificing}所有不符合筛选条件的符文吗？此操作将根据当前符文筛选设置，移除所有不符合筛选条件的符文。${negativeWarning}`;
     },
     extraMessage() {
-      if (this.glyphsDeleted === 0) return `This will remove no Glyphs.`;
-      if (this.glyphsDeleted === this.glyphsTotal) return `This will remove all your Glyphs.`;
-      return `This process will remove ${this.glyphsDeleted}/${this.glyphsTotal} Glyphs.`;
+      if (this.glyphsDeleted === 0) return `这将不会移除任何符文。`;
+      if (this.glyphsDeleted === this.glyphsTotal) return `这将移除全部符文。`;
+      return `这将移除 ${this.glyphsDeleted}/${this.glyphsTotal} 个符文。`;
     },
 
     // These two don't need to be reactive since the modal force-closes itself whenever glyphs change
