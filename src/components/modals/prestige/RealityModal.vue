d368da25ff4de341b9dfc705d2bdcddc5dd2275a
ABandonLoves <a_bandon_love@outlook.com>
2026-08-15 15:45:17 +0800

    汉化现实按钮

--- src/components/modals/prestige/RealityModal.vue
+++ src/components/modals/prestige/RealityModal.vue
@@ -31,46 +31,38 @@ export default {
   },
   computed: {
     firstRealityText() {
-      return `Reality will reset everything except Challenge records and anything under the General header on the
-        Statistics tab. The first ${formatInt(13)} rows of Achievements are also reset,
-        but you will automatically get one Achievement back every
-        ${timeDisplayNoDecimals(new Decimal(30 * 60000))}. You will also gain Reality Machines based on your Eternity Points, a
-        Glyph with a level based on your Eternity Points, Replicanti, and Dilated Time, a Perk Point to spend
-        on quality of life upgrades, and unlock various upgrades.`;
+      return `进行现实将重置所有的游戏内容以下除外：挑战纪录和统计页面中“概况”条目下的内容。 进行现实将重置前 ${formatInt(13)} 行的所有成就，但你将每隔 ${timeDisplayNoDecimals(new Decimal(30 * 60000))} 自动获得一个成就。现实之后将基于永恒点数获得现实机器，同时获得一个符文。符文的等级基于永恒点数、复制器和膨胀时间计算。你还能获得一个可用于提升游戏体验的复兴点数，并在第一次现实之后解锁大量的现实升级。`;
     },
     canSacrifice() {
       return RealityUpgrade(19).isEffectActive;
     },
     warnText() {
       if (!this.hasChoice) {
-        return `You currently only have a single option for new Glyphs every
-          Reality. You can unlock the ability to choose from multiple Glyphs by canceling out of this modal and
-          purchasing the START Perk.`;
+        return `每次现实时，你只有一个备选符文。你需要关闭这个弹窗，在复兴树中购买 START 节点，才能增加备选符文的数量。`;
       }
 
       if (this.hasFilter && this.selectedGlyph === undefined) {
-        return `If you do not choose a Glyph, one will be automatically selected using your Glyph filter.`;
+        return `若不选择符文，系统将根据你的符文筛选器自动选择。`;
       }
       return this.selectedGlyph === undefined
-        ? `You must select a Glyph in order to continue.`
+        ? `必须选择符文才能继续。`
         : null;
     },
     gained() {
       const gainedResources = [];
-      gainedResources.push(`${quantifyHybridLarge("Reality", this.simRealities)}`);
-      gainedResources.push(`${quantifyHybridLarge("Perk Point", this.simRealities)}`);
-      gainedResources.push(`${quantify("Reality Machine", this.realityMachines, 2)}`);
+      gainedResources.push(`${quantifyHybridLarge("现实次数", this.simRealities)}`);
+      gainedResources.push(`${quantifyHybridLarge("个复兴点数", this.simRealities)}`);
+      gainedResources.push(`${quantify("个现实机器", this.realityMachines, 2)}`);
       if (this.effarigUnlocked) {
-        gainedResources.push(`${quantify("Relic Shard", this.shardsGained, 2)}`);
+        gainedResources.push(`${quantify("个遗迹碎片", this.shardsGained, 2)}`);
       }
-      return `You will gain ${makeEnumeration(gainedResources)}`;
+      return `你将获得 ${makeEnumeration(gainedResources)}`;
     },
     levelStats() {
       // Bit annoying to read due to needing >, <, and =, with = needing a different format.
-      return `You will get a level ${formatHybridLarge(this.level, 3)} Glyph on Reality, which is
-        ${this.level.eq(this.bestLevel) ? "equal to" : `
-        ${quantifyHybridLarge("level", this.levelDifference)}
-        ${this.level.gt(this.bestLevel) ? "higher" : "lower"} than`} your best.`;
+      return `现实时获得一个等级为 ${formatHybridLarge(this.level, 3)} 的符文，它的等级
+        ${this.level.eq(this.bestLevel) ? "与你最高等级的符文相同" : `比你最高等级的符文
+        ${this.level.gt(this.bestLevel) ? "高" : "低"} ${quantifyHybridLarge("级", this.levelDifference)}。`}`;
     },
     confirmationToDisable() {
       return ConfirmationTypes.glyphSelection.isUnlocked() ? "glyphSelection" : undefined;
@@ -141,7 +133,7 @@ export default {
     @confirm="confirmModal(false)"
   >
     <template #header>
-      You are about to Reality
+      你将要进行一次现实
     </template>
     <div
       v-if="firstReality"
@@ -177,33 +169,27 @@ export default {
     </div>
     <div v-if="simRealities.gt(1)">
       <br>
-      After choosing this Glyph the game will simulate the rest of your Realities,
-      <br>
-      automatically choosing another {{ quantifyHybridSmall("Glyph", simRealities.sub(1)) }}
-      based on your Glyph filter settings.
+      选中该符文后游戏将计算剩余的现实，随后游戏将基于符文筛选的设置，自动选择另外 {{ quantifyHybridSmall("个符文", simRealities.sub(1)) }}。
     </div>
     <div v-if="willAutoPurge">
       <br>
-      Auto-purge is currently enabled; your selected Glyph
-      <br>
-      may not appear in your inventory after it triggers.
+      已启用符文仓库自动净化，选中的符文在自动净化触发后可能不会在符文仓库中出现。
     </div>
     <div
       v-if="!hasSpace"
       class="o-warning"
     >
       <span v-if="simRealities.gt(1)">
-        You will be simulating more Realities than you have open inventory space for;
-        this may result in some Glyphs being Sacrificed.
+        即将执行的现实重置次数超过剩余仓库槽位；这可能导致部分符文被自动净化。
       </span>
       <span v-else>
-        You do not have any free inventory space - your selected Glyph will be automatically
-        {{ canSacrifice ? "Sacrificed" : "deleted" }}!
+        你的符文仓库没有可用空间，选中的符文将被
+        {{ canSacrifice ? "献祭" : "移除" }}！
       </span>
     </div>
     <div v-if="confirmationToDisable">
       <br>
-      You can force this modal to appear (even if disabled) by Shift-clicking the Reality button.
+      按住 Shift 键点击现实按钮可强制显示此弹窗（即使已禁用）。
     </div>
     <template
       v-if="canSacrifice && canConfirm"
@@ -213,7 +199,7 @@ export default {
         class="o-primary-btn--width-medium c-modal-message__okay-btn"
         @click="confirmModal(true)"
       >
-        Sacrifice
+        献祭
       </PrimaryButton>
     </template>
   </ModalWrapperChoice>
