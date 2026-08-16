bf41546ccc2ffb3d60bb72df698bbe1d10312a68
ABandonLoves <a_bandon_love@outlook.com>
2026-08-16 21:58:58 +0800

    天界永恒点数文本问题

--- src/components/tabs/celestial-dimension-expansion/CEPMultiplierButton.vue
+++ src/components/tabs/celestial-dimension-expansion/CEPMultiplierButton.vue
@@ -53,7 +53,7 @@ export default {
         当前: {{ formatX(multiplier, 2, 0) }}
       </div>
       <br>
-      价格: {{ quantify("Celestial Eternity Point", cost, 2, 0) }}
+      价格: {{ quantify("天界永恒点数", cost, 2, 0) }}
     </button>
     <PrimaryButton
       class="l--spoon-btn-group__little-spoon o-primary-btn--small-spoon"
